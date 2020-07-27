366d80e521d58912697cfc044d1a7fe383995cc2
kolychka <randytaylor.fckoff@gmail.com>
2020-07-27 20:08:10 +0500

    Refactoring pug loading and fix loading images

--- webpack.config.js
+++ webpack.config.js
@@ -5,16 +5,18 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimazeCssAssetPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
-
 const isDev = process.env.NODE_ENV === 'development'
 const isProd = !isDev
 
 const optimization = () => {
 	const config = {
-		splitChunks: {
+
+		/*splitChunks: {
 			chunks: 'all'
 			}
+		*/
 		}
+		
 	if (isProd) {
 		config.minimizer = [
 			new OptimazeCssAssetPlugin(),
@@ -36,7 +38,7 @@ const CssLoaders = (extra) => {
 			loader: MiniCssExtractPlugin.loader,
 			options: {
 				hmr: isDev,
-				reloadAll:true,
+				reloadAll:true
 			},
 		}, 'css-loader'
 	]
@@ -55,8 +57,8 @@ module.exports = {
 
 	entry: {
 		index: './entry/index.js',
-		uikit: './entry/ui-kit.js'
-		
+		uikit: './entry/ui-kit.js',
+		uikit2: './entry/ui-kit2.js'
 	},
 
 	output: {
@@ -67,7 +69,8 @@ module.exports = {
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, 'src'),
-			'@components': path.resolve(__dirname, 'src/components')
+			'@components': path.resolve(__dirname, 'src/components'),
+			images: path.resolve(__dirname, 'src/assets/img')
 		}
 	},
 
@@ -76,23 +79,20 @@ module.exports = {
 	devServer: {
 		port: 8081,
 		hot: isDev,
-		index: 'ui-kit.html',
-		stats: 'errors-only'
+		index: 'ui-kit2.html',
 	},
 	
 	plugins: [
 		new HTMLWebpackPlugin({
 			template: './pages/ui-kit.pug',
 			filename: filename('ui-kit', 'html'),
-			chunks: 'uikit',
+			chunks: ['uikit'],
 		}),
-		/*
 		new HTMLWebpackPlugin({
-			template: './pages/about.pug',
-			filename: filename('about', 'html'),
-			chunks: ['about'],
+			template: './pages/ui-kit2.pug',
+			filename: filename('ui-kit2', 'html'),
+			chunks: ['uikit2'],
 		}),
-		*/
 		new CleanWebpackPlugin(),
 		new MiniCssExtractPlugin({
 			filename: filename('[name]', 'css'),
@@ -114,27 +114,31 @@ module.exports = {
 				use: CssLoaders('sass-loader'),
 			},
 			{
-				test: /\.pug$/,
-				use: [{
-					loader: 'raw-loader',
-				}, 
-				{
-					loader: 'pug-plain-loader',
-					options: {
-						hmr: isDev,
-						reloadAll:true,
-						pretty: true,
-					}
-				}],
+				test: /\.pug$/,			
+				loader: 'pug-loader',
+				options: {
+					hmr: isDev,
+					reloadAll:true,
+					pretty: true,
+				}
 			},
 			{
 				test: /\.(png|jpg|svg|gif)$/,
-				use: ['file-loader']
+				loader: 'file-loader', 
+				options: {
+					name: '[name].[ext]',
+					outputPath: 'assets/images/'
+				}
 			},
 			{
 				test: /\.(ttf|woff|woff2|eot)$/,
-				use: ['file-loader']
+				loader: 'file-loader',
+				options: {
+					name: '[name].[ext]',
+					outputPath: 'assets/fonts/'
+				}
 			}
 		]
-	}
+	},
 }
+
